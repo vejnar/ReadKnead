a59f59d7b77f5e8cc070b3eeac4b3f6f3e20c67c
vejnar <charles.vejnar@gmail.com>
2023-03-10 11:44:36 -0500

    Switch to single year copyright

--- lib/operations/length.go
+++ lib/operations/length.go
@@ -1,5 +1,5 @@
 //
-// Copyright (C) 2017-2022 Charles E. Vejnar
+// Copyright © 2017 Charles E. Vejnar
 //
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v. 2.0. If a copy of the MPL was not distributed with this
